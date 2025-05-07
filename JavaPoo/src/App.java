76ae35d7a0eed2bae969dcac4ad7751773c15459
AlissonVinic <alissonvincns@gmail.com>
2025-05-07 20:01:29 -0300

    Aula de Poo - tarefas

--- JavaPoo/src/App.java
+++ JavaPoo/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Arquivo java no repositório");
+        Viagem v1 = new Viagem("Torre Eifel, França.", 4);
+        Viagem v2 = new Viagem("Alisson", "Paris", 200, 300, 150, 3, 4);
+        v1.exibir("Alisson");
+        System.out.println("----------------------------");
+        v2.exibir();
+        System.out.println("----------------------------");
+        System.out.printf("Custo total: %.2f\n", v2.calcCusto());
+
     }
 }
